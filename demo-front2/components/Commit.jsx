5c7674441997d6c606015c2dd5656d271ac28df4
지의신 <64007655+usgeeus@users.noreply.github.com>
2024-01-03 11:28:29 +0900

    add toggle for input to be hex or decimal

--- demo-front2/components/Commit.jsx
+++ demo-front2/components/Commit.jsx
@@ -12,18 +12,22 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 import { useWeb3Contract } from "react-moralis"
+import { Switch } from "@headlessui/react"
 import dynamic from "next/dynamic"
 import { abi, contractAddresses } from "./../constants"
 import { useMoralis } from "react-moralis"
 import { useEffect, useState } from "react"
 import { createTestCases2 } from "./../utils/testFunctions"
-import { Input, useNotification, Button } from "web3uikit"
+import { Input, useNotification, Button, Toggle } from "web3uikit"
 import { getBitLenth2, getLength } from "../utils/testFunctions"
 import { toBeHex, dataLength, ethers } from "ethers"
 import React from "react"
 const ReactJson = dynamic(() => import("react-json-view-with-toggle"), {
     ssr: false,
 })
+function classNames(...classes) {
+    return classes.filter(Boolean).join(" ")
+}
 export default function Commit() {
     const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
     const chainId = parseInt(chainIdHex)
@@ -32,7 +36,9 @@ export default function Commit() {
     const [commitData, setCommitData] = useState()
     const [commitDataState, setCommitDataState] = useState("initial")
     const { runContractFunction: enterRafByCommit, isLoading, isFetching } = useWeb3Contract()
+    const [enabled, setEnabled] = useState(false)
     const dispatch = useNotification()
+
     function validation() {
         if (commitData == undefined || commitData == "") {
             setCommitDataState("error")
@@ -68,14 +74,15 @@ export default function Commit() {
                         message:
                             error?.error?.message && error.error.message != "execution reverted"
                                 ? error.error.message
-                                : new ethers.Interface(abi).parseError(
+                                : error.error
+                                ? new ethers.Interface(abi).parseError(
                                       error.error.data.originalError.data
-                                  ).name,
+                                  ).name
+                                : error?.data?.message,
                         title: "Error Message",
                         position: "topR",
                         icon: "bell",
                     })
-                    console.log(error)
                 },
             })
         }
@@ -105,9 +112,38 @@ export default function Commit() {
             <h3 data-testid="test-form-title" className="sc-eXBvqI eGDBJr font-bold">
                 Join Christmas Event by Commit
             </h3>
+            <div className="mb-2 mt-2 flex flex-row">
+                <div className="mx-1.5">Hex</div>
+                <Switch
+                    checked={enabled}
+                    onChange={setEnabled}
+                    className="group relative inline-flex h-5 w-10 flex-shrink-0 cursor-pointer items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2"
+                >
+                    <span className="sr-only">Use setting</span>
+                    <span
+                        aria-hidden="true"
+                        className="pointer-events-none absolute h-full w-full rounded-md bg-white"
+                    />
+                    <span
+                        aria-hidden="true"
+                        className={classNames(
+                            enabled ? "bg-indigo-600" : "bg-emerald-600",
+                            "pointer-events-none absolute mx-auto h-4 w-9 rounded-full transition-colors duration-200 ease-in-out"
+                        )}
+                    />
+                    <span
+                        aria-hidden="true"
+                        className={classNames(
+                            enabled ? "translate-x-5" : "translate-x-0",
+                            "pointer-events-none absolute left-0 inline-block h-5 w-5 transform rounded-full border border-gray-200 bg-white shadow ring-0 transition-transform duration-200 ease-in-out"
+                        )}
+                    />
+                </Switch>
+                <div className="mx-1">Decimal</div>
+            </div>
             <div className="mb-2 mt-5 flex flex-row">
                 <Input
-                    label="Commit Value in Decimal"
+                    label={enabled ? "Commit Value in Decimal" : "Commit Value in Hex"}
                     type="text"
                     placeholder=""
                     id="CommitValue"
@@ -115,12 +151,17 @@ export default function Commit() {
                     value={commitData}
                     onChange={(e) => {
                         setCommitData(e.target.value)
-                        let stringVal = e.target.value
-                        if (e.target.value.length == 0) stringVal = "0"
-                        setCommitCalldata({
-                            val: toBeHex(stringVal, getLength(dataLength(toBeHex(stringVal)))),
-                            bitlen: getBitLenth2(BigInt(stringVal)),
-                        })
+                        if (enabled) {
+                            let stringVal = e.target.value
+                            if (e.target.value.length == 0) stringVal = "0"
+                            setCommitCalldata({
+                                val: toBeHex(stringVal, getLength(dataLength(toBeHex(stringVal)))),
+                                bitlen: getBitLenth2(BigInt(stringVal)),
+                            })
+                        } else {
+                            if (e.target.value.length != 0)
+                                setCommitCalldata(JSON.parse(e.target.value))
+                        }
                     }}
                     state={commitDataState}
                     errorMessage="Commit Value is required"
@@ -134,7 +175,17 @@ export default function Commit() {
                             (o, v) => o + ("00" + v.toString(16)).slice(-2),
                             ""
                         )
-                        setCommitData(BigInt("0x" + bytesHex).toString(10))
+                        if (enabled) setCommitData(BigInt("0x" + bytesHex).toString(10))
+                        else
+                            setCommitData(
+                                JSON.stringify({
+                                    val: toBeHex(
+                                        "0x" + bytesHex,
+                                        getLength(dataLength("0x" + bytesHex))
+                                    ),
+                                    bitlen: getBitLenth2(BigInt("0x" + bytesHex)),
+                                })
+                            )
                         let stringVal = BigInt("0x" + bytesHex).toString(10)
                         if (stringVal.length == 0) stringVal = "0"
                         setCommitCalldata({
